c08b48f580144d6021f85a162057bc3c2be7977c
Dalibor Dragojevic <daco.dragojevic@gmail.com>
2018-05-01 15:38:24 +0200

    optimistic update with revert completed

--- src/components/TimersDashboard.js
+++ src/components/TimersDashboard.js
@@ -6,28 +6,52 @@ import PersistenceService from '../services/PersistenceService';
 import EditableTimerList from './EditableTimerList';
 import ToggleableTimerForm from './ToggleableTimerForm';
 
+
+// setState updater
+function setTimer(timers, timerId, oldTimerValue) {
+    const now = Date.now();
+
+    return timers.map((timer) => {
+        if (timer.id === timerId) {
+            if (oldTimerValue) {
+                return oldTimerValue;
+            }
+            else {
+                const lastElapsed = now - timer.runningSince;
+                return Object.assign({}, timer, {
+                    elapsed: timer.elapsed + lastElapsed,
+                    runningSince: null,
+                });
+            }
+        } else {
+            return timer;
+        }
+    });
+}
+
 export default class TimersDashboard extends React.Component {
-    state = {timers: []};
+    state = { timers: [] };
     static persistenceService = new PersistenceService;
-/*        timers: [
-          {
-            title: 'Practice squat',
-            project: 'Gym Chores',
-            id: uuid.v4(),
-            elapsed: 1,
-            runningSince: Date.now(),
-    }, {
-            title: 'Bake squash',
-            project: 'Kitchen Chores',
-            id: uuid.v4(),
-            elapsed: 1,
-            runningSince: null,
-    }, ],
-    }; */
+    static oldTimerValue = {};
+    /*        timers: [
+              {
+                title: 'Practice squat',
+                project: 'Gym Chores',
+                id: uuid.v4(),
+                elapsed: 1,
+                runningSince: Date.now(),
+        }, {
+                title: 'Bake squash',
+                project: 'Kitchen Chores',
+                id: uuid.v4(),
+                elapsed: 1,
+                runningSince: null,
+        }, ],
+        }; */
 
     componentDidMount() {
         TimersDashboard.persistenceService.loadTimers().then((loadedTimers) => {
-            this.setState({timers: loadedTimers});
+            this.setState({ timers: loadedTimers });
         })
     }
 
@@ -42,75 +66,64 @@ export default class TimersDashboard extends React.Component {
     handleTrashClick = (timerId) => {
         this.deleteTimer(timerId);
     };
-    
+
     createTimer = (timer) => {
-        const t = { title: timer.title, project: timer.project, id: uuid.v4(), elapsed: 1, runningSince: null, } 
+        const t = { title: timer.title, project: timer.project, id: uuid.v4(), elapsed: 1, runningSince: null, }
         TimersDashboard.persistenceService.updateTimer(t, this.state.timers.length, t)
-        .then(() => TimersDashboard.persistenceService.loadTimers())
-        .then((loadedTimers) => {
-            this.setState({
-              timers: loadedTimers,
-            });
-        })
+            .then(() => TimersDashboard.persistenceService.loadTimers())
+            .then((loadedTimers) => {
+                this.setState({
+                    timers: loadedTimers,
+                });
+            })
     };
 
     updateTimer = (attrs) => {
         this.setState({
-          timers: this.state.timers.map((timer) => {
-            if (timer.id === attrs.id) {
-              return Object.assign({}, timer, {
-                title: attrs.title,
-                project: attrs.project,
-    });
-    } else {
-              return timer;
-            }
-    }), });
+            timers: this.state.timers.map((timer) => {
+                if (timer.id === attrs.id) {
+                    return Object.assign({}, timer, {
+                        title: attrs.title,
+                        project: attrs.project,
+                    });
+                } else {
+                    return timer;
+                }
+            }),
+        });
     };
 
     deleteTimer = (timerId) => {
         this.setState({
-          timers: this.state.timers.filter(t => t.id !== timerId),
+            timers: this.state.timers.filter(t => t.id !== timerId),
         });
     };
 
     handleStartClick = (timerId) => {
         this.startTimer(timerId);
     };
-      handleStopClick = (timerId) => {
+    handleStopClick = (timerId) => {
         this.stopTimer(timerId);
     };
 
     startTimer = (timerId) => {
         const now = Date.now();
         this.setState({
-          timers: this.state.timers.map((timer) => {
-            if (timer.id === timerId) {
-              return Object.assign({}, timer, {
-                 runningSince: now,
-            });
-          } else {
-            return timer;
-    } }),
-    }); };
+            timers: this.state.timers.map((timer) => {
+                if (timer.id === timerId) {
+                    TimersDashboard.oldTimerValue = timer;
+                    return Object.assign({}, timer, {
+                        runningSince: now,
+                    });
+                } else {
+                    return timer;
+                }
+            }),
+        });
+    };
 
     stopTimer = (timerId) => {
         const now = Date.now();
-
-        this.setState({
-            timers: this.state.timers.map((timer, timerIndex) => {
-              if (timer.id === timerId) {
-                const lastElapsed = now - timer.runningSince;
-                TimersDashboard.persistenceService.updateTimer(timer, timerIndex, {elapsed: timer.elapsed + lastElapsed, runningSince: null });
-                return Object.assign({}, timer, {
-                  elapsed: timer.elapsed + lastElapsed,
-                  runningSince: null,
-                });
-              } else {
-                return timer;
-              }
-            }),
-        });
 
         let timerIndex = 0;
         for (let timer of this.state.timers) {
@@ -120,12 +133,20 @@ export default class TimersDashboard extends React.Component {
             }
 
             const lastElapsed = now - timer.runningSince;
-            TimersDashboard.persistenceService.updateTimer(timer, timerIndex, {elapsed: timer.elapsed + lastElapsed, runningSince: null })
-            .then(() => TimersDashboard.persistenceService.loadTimers())
-            .then((loadedTimers) => {
-                this.setState({timers: loadedTimers});
-            })
+            TimersDashboard.persistenceService.updateTimer(timer, timerIndex, { elapsed: timer.elapsed + lastElapsed, runningSince: null })
+                .then(() => TimersDashboard.persistenceService.loadTimers())
+                .then((loadedTimers) => {
+                    this.setState({ timers: loadedTimers });
+                })
+                .catch(() => {
+                    this.setState({ timers: setTimer(this.state.timers, timerId, TimersDashboard.oldTimerValue) });
+                })
+
+            break;
         }
+
+        this.setState({ timers: setTimer(this.state.timers, timerId) });
+
     };
 
     render() {
@@ -133,8 +154,8 @@ export default class TimersDashboard extends React.Component {
             <div className='ui three column centered grid'>
                 <div className='column'>
                     <EditableTimerList timers={this.state.timers} onFormSubmit={this.handleEditFormSubmit}
-                                        onTrashClick={this.handleTrashClick}
-                                        onStartClick={this.handleStartClick} onStopClick={this.handleStopClick} />
+                        onTrashClick={this.handleTrashClick}
+                        onStartClick={this.handleStartClick} onStopClick={this.handleStopClick} />
                     <ToggleableTimerForm onFormSubmit={this.handleCreateFormSubmit} />
                 </div>
             </div>
--- src/services/PersistenceService.js
+++ src/services/PersistenceService.js
@@ -3,21 +3,27 @@ export default class TimersPersistence {
     updateTimer(timer, timerIndex, toUpdate) {
         return new Promise((resolve, reject) => {
             setTimeout(() => {
-                const timerKeyString = `timer-${timerIndex}`
-
-                const titleKeyString = timerKeyString + 'title';
-                const projectKeyString = timerKeyString + 'project';
-                const idKeyString = timerKeyString + 'id';
-                const elapsedKeyString = timerKeyString + 'elapsed';
-                const runningSinceKeyString = timerKeyString + 'runningSince';
                 
-                localStorage.setItem(titleKeyString, toUpdate.title ? toUpdate.title : timer.title);
-                localStorage.setItem(projectKeyString, toUpdate.project ? toUpdate.project : timer.project);
-                localStorage.setItem(idKeyString, toUpdate.id ? toUpdate.id : timer.id);
-                localStorage.setItem(elapsedKeyString, toUpdate.elapsed ? toUpdate.elapsed : timer.elapsed);
-                localStorage.setItem(runningSinceKeyString, toUpdate.runningSince);
+                if (timerIndex === 0) {
+                    const timerKeyString = `timer-${timerIndex}`
 
-                resolve(true);
+                    const titleKeyString = timerKeyString + 'title';
+                    const projectKeyString = timerKeyString + 'project';
+                    const idKeyString = timerKeyString + 'id';
+                    const elapsedKeyString = timerKeyString + 'elapsed';
+                    const runningSinceKeyString = timerKeyString + 'runningSince';
+                    
+                    localStorage.setItem(titleKeyString, toUpdate.title ? toUpdate.title : timer.title);
+                    localStorage.setItem(projectKeyString, toUpdate.project ? toUpdate.project : timer.project);
+                    localStorage.setItem(idKeyString, toUpdate.id ? toUpdate.id : timer.id);
+                    localStorage.setItem(elapsedKeyString, toUpdate.elapsed ? toUpdate.elapsed : timer.elapsed);
+                    localStorage.setItem(runningSinceKeyString, toUpdate.runningSince);
+                    
+                    resolve(true);
+                }
+                else {
+                    reject();
+                }
             }, 500)
         });
     };
